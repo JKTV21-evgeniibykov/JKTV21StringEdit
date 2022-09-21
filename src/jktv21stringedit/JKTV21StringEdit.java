730b679a55cc4c312965bff4788da5e91e368c7f
pupil <pupil@ivkhk.ee>
2022-09-21 10:40:13 +0300

    first commit

---
+++ src/jktv21stringedit/JKTV21StringEdit.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jktv21stringedit;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pupil
+ */
+public class JKTV21StringEdit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Enter text: ");
+        Scanner scanner = new Scanner(System.in);
+        java.lang.String str = scanner.nextLine();
+    }
+    
+}
