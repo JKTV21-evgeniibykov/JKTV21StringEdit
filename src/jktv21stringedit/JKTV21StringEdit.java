4e538a74a089e5bf3380fb753ea9e143fdd49ff2
pupil <pupil@ivkhk.ee>
2022-09-22 08:34:43 +0300

    second commit

--- src/jktv21stringedit/JKTV21StringEdit.java
+++ src/jktv21stringedit/JKTV21StringEdit.java
@@ -43,6 +43,23 @@ public class JKTV21StringEdit {
                     String[] words = str.split(" ");
                     System.out.println("Words = " + words.length);
                     break;
+                case "3":
+                    System.out.print("Find word: ");
+                    String findWord = scanner.nextLine ();
+                    int i = str.indexOf(findWord);
+                    if(i > 0)
+                        System.out.println("Find word = " + str.substring(i, i + findWord.length()));
+                    else 
+                        System.out.println("String not found");
+                    break;
+                case "4":
+                    System.out.print("Find word: ");
+                    String rWord1 = scanner.nextLine ();
+                    System.out.print("Replace word: ");
+                    String rWord2 = scanner.nextLine ();
+                    str = str.replace(rWord1, rWord2);
+                    System.out.println("Change string = " + str);
+                    break;
                 default:
                     break;
             }
